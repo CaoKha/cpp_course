40bd935e5059119e88798b307444b393dd8d05a4
Kha Nguyen Cao <nguyencaokha131995@gmail.com>
2021-03-08 15:37:05 +0100

    commit

--- src/io_stream.cpp
+++ src/io_stream.cpp
@@ -19,23 +19,33 @@ std::ostream &operator<<(std::ostream &os, const A &v)
 }
 int main()
 {
-    print(std::cout);
-    std::cout << std::endl;
-    std::ofstream my_file("test");
-    print(my_file);
-    my_file.close();
-    std::cout << "saving the file";
-    std::ostringstream ss;
-    print(ss);
-    std::cout << " in string " << ss.str();
-    std::cout << std::endl;
 
+    //testing with iostream
     int my_var = 5;
     A my_data{4};
-    std::cout << "coucou" << my_var << " " << my_data << std::endl;
-    char cc = '13';
+    std::cout << "my_var = " << my_var << " and my_data= " << my_data << std::endl;
+    char cc = '9';
+    std::cout << "this is output using std::cout.put: ";
     std::cout.put(cc);
-
+    std::cout.write("\n", 1); // "a" is const char , 'a' is char
     char tabchar[] = {'f', 'a', 'b', 'i', 'e', 'n'};
-    std::cout.write(tabchar, 60);
+    std::cout << "this is output using std::cout.write: ";
+    std::cout.write(tabchar, 6);
+    std::cout.put('\n');
+
+    //testing with ofstream
+    std::ofstream output_file("test");
+    output_file << "coucou" << my_var << " " << my_data << std::endl;
+    output_file.put(cc);
+    output_file.write(tabchar, 6);
+    output_file.close();
+
+    //testing with ifstream
+    std::ifstream entry("test2");
+    std::string line;
+    while (std::getline(entry, line))
+    {
+        std::cout << "this is new line : " << line << '\n';
+    }
+    entry.close();
 }
