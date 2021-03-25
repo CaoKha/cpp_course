a808f2be2c253d9a56c975f4f5b7ed80751c1b27
CaoKha <nguyencaokha131995@gmail.com>
2021-03-25 16:53:36 +0100

    commit

--- src/typedef_example.cpp
+++ src/typedef_example.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <string>
-typedef enum days
+#include <ostream>
+enum days
 {
     SUN,
     MON,
@@ -9,15 +10,44 @@ typedef enum days
     THU,
     FRI,
     SAT
-} days;
+};
 inline days operator++(days d)
 {
     return static_cast<days>((static_cast<int>(d) + 1) % 7);
 }
 
+std::ostream &operator<<(std::ostream &out, const days &d)
+{
+    switch (d)
+    {
+    case SUN:
+        out << "SUN";
+        break;
+    case MON:
+        out << "MON";
+        break;
+    case TUE:
+        out << "TUE";
+        break;
+    case WED:
+        out << "WED";
+        break;
+    case THU:
+        out << "THU";
+        break;
+    case FRI:
+        out << "FRI";
+        break;
+    case SAT:
+        out << "SAT";
+        break;
+    }
+    return out;
+}
+
 int main()
 {
-    days d(SUN);
+    days d(MON);
     d = ++d;
     std::cout << d << std::endl;
 }
